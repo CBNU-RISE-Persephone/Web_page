d97f2f7ea52ccbc6157777bea5198113b20ca6f6
Kim-jiseong <kimjs030727@gmail.com>
2026-05-25 05:50:24 +0900

    feat: add Contact Us page styling

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,6 +1,33 @@
+import '../styles/pages/ContactUs.scss'
+
 function ContactUs(){
     return(
-        <p>연락수단</p>
+        <main className='ContactUsPage'>
+
+        <div className="headBox">
+            <h3>Contact Us</h3>
+        </div>
+
+        <form className="contactForm">
+            <div className='topInputGroup'>
+                <div className='formField'>
+                    <label for='name'>Name</label>
+                    <input id='name' type='text' placeholder='Please enter your name'></input>
+                </div>
+                <div className='formField'>
+                    <label for='email'>E-mail</label>
+                    <input id='email' type='email' placeholder='Please enter email'></input>
+                </div>
+            </div>
+            <div className='formField contentsField'>
+                <label for='contents'>Contents</label>
+                <textarea id='contents' placeholder='Contents...'></textarea>
+            </div>
+
+            <button type='submit' className='sendBtn'>Send</button>
+        </form>
+
+        </main>
     );
 }
 
