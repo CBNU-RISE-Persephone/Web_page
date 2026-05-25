682c644e0af2fd7ce19ca0ec08ec0286639b8e74
Kim-jiseong <kimjs030727@gmail.com>
2026-05-25 23:31:24 +0900

    feat: add detail styles for Team page
    - add profile img section and Dummy Img
      개인 소개 이미지 공간추가 및 더미 이미지(추후 변경)
    - TODO: So bad bad design. need minsuk's touch :(
            add detail info component and onclick function.
      할 일: 디자인 매우 매우 나쁨. 민석이의 디자인 터지 필요:(
            상세 정보창 컴포넌트 추가 및 onclick 구현할 것

--- src/Pages/Teams.jsx
+++ src/Pages/Teams.jsx
@@ -1,4 +1,7 @@
 import '../styles/pages/Teams.scss'
+import songImg from '../Assets/Images/teamImg/song_tae_yang.jpg'
+import micImg from '../Assets/Images/teamImg/mickseogi.jpg'
+import kimImg from '../Assets/Images/teamImg/kim_ji_seong.jpg'
 
 function Teams(){
     return(
@@ -11,24 +14,31 @@ function Teams(){
             </div>
         </div>
 
+        {/* Todo: 각 버튼들(infoCard) onclick 및 함수 구현해야함.
+            정보 띄우는 창은 별도 컴포넌트로 구현.
+            +) 매우매우매우 나쁜 디자인. 민석이의 손을 좀 봐야할 듯 */}
+            
         <div className='contentBox'>
-            <div className='infoBox'>
-                <div className='profileImg'>
-                    
+            <button className='infoCard'>
+                <div className='imgBox'>
+                    <img src={songImg} alt='teamImg_01'></img>
                 </div>
-            </div>
+                <h3>송태양</h3>
+            </button>
 
-            <div className='infoBox'>
-                <div className='profileImg'>
-
+            <button className='infoCard'>
+                <div className='imgBox'>
+                    <img src={micImg} alt='teamImg_02'></img>
                 </div>
-            </div>
+                <h3>서민석</h3>
+            </button>
 
-            <div className='infoBox'>
-                <div className='profileImg'>
-
+            <button className='infoCard'>
+                <div className='imgBox'>
+                    <img src={kimImg} alt='teamImg_03'></img>
                 </div>
-            </div>
+                <h3>김지성</h3>
+            </button>
         </div>
 
         </main>
