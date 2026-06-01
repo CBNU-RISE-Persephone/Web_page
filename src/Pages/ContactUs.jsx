5f083345b188710ca3ee419a23e4c77f96ec514b
Kim-jiseong <kimjs030727@gmail.com>
2026-06-01 20:52:38 +0900

    fix: edit ContactUs page layout and design

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -11,16 +11,16 @@ function ContactUs(){
         <form className="contactForm">
             <div className='topInputGroup'>
                 <div className='formField'>
-                    <label for='name'>Name</label>
+                    <label htmlFor='name'>Name</label>
                     <input id='name' type='text' placeholder='Please enter your name'></input>
                 </div>
                 <div className='formField'>
-                    <label for='email'>E-mail</label>
+                    <label htmlFor='email'>E-mail</label>
                     <input id='email' type='email' placeholder='Please enter email'></input>
                 </div>
             </div>
             <div className='formField contentsField'>
-                <label for='contents'>Contents</label>
+                <label htmlFor='contents'>Contents</label>
                 <textarea id='contents' placeholder='Contents...'></textarea>
             </div>
 
